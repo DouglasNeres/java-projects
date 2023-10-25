f6aa22688e05597ecc42853689822aeb432e5f9c
Douglas Neres da S <douglasneres06@gmail.com>
2023-10-25 15:23:24 -0300

    PUT route update and creation of Utils.java

--- todolist/src/main/java/javaproject/todolist/task/TaskController.java
+++ todolist/src/main/java/javaproject/todolist/task/TaskController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import javaproject.todolist.utils.Utils;
 
   @RestController
   @RequestMapping("/tasks")
@@ -26,7 +27,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
   @PostMapping("/")
   public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-    System.out.println("Chegou no Controller");
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
 
@@ -53,9 +53,11 @@ import jakarta.servlet.http.HttpServletRequest;
   
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request , @PathVariable UUID id) {
-    var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUser);
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
